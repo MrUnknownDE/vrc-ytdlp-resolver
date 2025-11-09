a7e4f8545d2f5c599562c607938ab813b859befa
MrUnknownDE <me@johanneskr.de>
2025-11-09 13:24:48 +0100

    set trust proxy

--- src/server.js
+++ src/server.js
@@ -10,6 +10,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
+app.set('trust proxy', true);
 app.use(morgan("tiny"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
